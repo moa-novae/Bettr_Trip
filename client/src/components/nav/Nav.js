357d1c428c5f9d55445d5452e2cceb5551595d61
aseenontv1027 <kkktkaten@hotmail.com>
2020-02-18 17:54:00 -0500

    react-router implemented

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 export default function() {
   return (
     <nav>
-      <h3>Logo</h3>
+      <Link to='/'>
+        <h3>Logo</h3>
+      </Link>
       <ul className="nav-links">
-        <li>About</li>
-        <li>Get Started</li>
-        <li>Sign Up</li>
-        <li>Log In</li>
+        <Link to='/about'>
+          <li>About</li>
+        </Link>
+        <Link to='/trips'>
+          <li>Get Started</li>
+        </Link>
+        <Link to='/signup'>
+          <li>Sign Up</li>
+        </Link>
+        <Link to='/login'>
+          <li>Log In</li>
+        </Link>
       </ul>
     </nav>
   );
