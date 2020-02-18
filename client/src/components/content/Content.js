17a6319b853047ce979d90012f808f06885092d7
JashanB <jashan_brar50@hotmail.com>
2020-02-17 20:48:59 -0500

    refactor map

--- client/src/components/content/Content.js
+++ client/src/components/content/Content.js
@@ -12,7 +12,14 @@ class Content extends Component {
           <Calendar />
         </div>
         <div className="map-container">
-          <MapWithASearchBox />
+          <MapWithASearchBox
+            lat={41.9}
+            lng={-87.624}
+            googleMapURL= {`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            />
         </div>
       
       </div>
---
+++ client/src/components/searchItem/SearchItem.js
@@ -0,0 +1,23 @@
+// import React, {useState} from "react";
+
+// export default function SearchItem(props) {
+//   //map through props, creating a indivual form for each 
+//   const results = props.placesData.map((place) => {
+//     const[state, setState] = useState("")
+    
+//     return (
+//     <li className="search-item">
+//       <h2 className="search-place">{place.name}</h2>
+//       <h3 className="search-place">{place.category}</h3>
+//       <button class="favorite">{img}</button>
+//    </li>
+//     )
+
+//   })
+//   return (
+//     <div className="searchBar">
+//       results
+//     </div>
+//   )
+
+// }
