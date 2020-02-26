83cd350027b2fd6aea9bd2ad07307ea8f130a418
aseenontv1027 <kkktkaten@hotmail.com>
2020-02-26 13:29:06 -0500

    latest

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Trip.create(id: 1, name: "Fun Trip", start_date: "2020-03-1", end_date: '2020-03
 
 Point.create(name: "Fairview Mall", latitude: 43.778191, longitude: -79.344116, start_time: '2020-03-01 08:15:41', end_time: '2020-03-01 10:15:41', trip_id: 1, activity: 'somethings', travel_method: 'driving', travel_duration: 2)
 Point.create(name: "Eaton Center", latitude: 43.654640, longitude: -79.380732, start_time: '2020-03-01 12:15:41', end_time: '2020-03-01 14:15:41', trip_id: 1, activity: 'icecream', travel_method: 'walking', travel_duration: 20)
-Point.create(name: "CN Tower", latitude: 43.642706, longitude: -79.387078, start_time: '2020-03-01 15:15:41', end_time: '2020-03-01 17:15:41', trip_id: 1, activity: 'sightsee', travel_method: 'bicycling', travel_duration: 12)
+Point.create(name: "CN Tower", latitude: 43.642706, longitude: -79.387078, start_time: '2020-03-01 15:15:41', end_time: '2020-03-01 17:15:41', trip_id: 1, activity: 'sightsee', travel_method: 'driving', travel_duration: 12)
 Point.create(name: "Lighthouse Lab", latitude: 43.644701, longitude: -79.401970, start_time: '2020-03-02 10:15:41', end_time: '2020-03-02 12:15:41', trip_id: 1, activity: 'nothing', travel_method: 'driving', travel_duration: 2)
 Point.create(name: "Fairview Mall", latitude: 43.778191, longitude: -79.344116, start_time: '2020-03-02 14:15:41', end_time: '2020-03-02 17:15:41', trip_id: 1, activity: 'somethings', travel_method: 'walking', travel_duration: 2)
 Point.create(name: "Houston", latitude: 29.7604267, longitude: -95.3698028, start_time: '2020-03-02 18:15:41', end_time: '2020-03-02 20:15:41', trip_id: 1, activity: 'dunno', travel_method: 'Car', travel_duration: 2)
@@ -30,5 +30,3 @@ Point.create(name: "Vancouver", latitude: 49.2827, longitude: 123.1207, start_ti
 # Point.create(name: "Houston", latitude: 29.7604267, longitude: -95.3698028, start_time: '2020-02-20 04:00:41', end_time: '2020-02-20 05:15:41', trip_id: 2)
 Point.create(name: "Houston", latitude: 29.7604267, longitude: -95.3698028, start_time: '2020-02-20 02:15:41', end_time: '2020-02-20 03:15:41', trip_id: 2)
 Point.create(name: "Houston", latitude: 29.7604267, longitude: -95.3698028, start_time: '2020-02-20 04:00:41', end_time: '2020-02-20 05:15:41', trip_id: 2)
-
-User.create!(email: "abc@yahoo.com.tw", password: "1", password_confirmation: "1")
